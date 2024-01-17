709071d6557d6e7396e6213f2fc0cf86a1c5ce9b
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-17 18:14:16 +0100

    Set up custom properties for text and background colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        "light-primary": "hsl(var(--color-bg-light) / 1)",
+        "light-secondary": "hsl(var(--color-elem-light) / 1)",
+        "dark-primary": "hsl(var(--color-bg-dark) / 1)",
+        "dark-secondary": "hsl(var(--color-elem-dark) / 1)",
+      },
+      textColor: {
+        "light-primary": "hsl(var(--color-text-light) / 1)",
+        "light-secondary": "hsl(var(--color-input-light) / 1)",
+        dark: "hsl(var(--color-text-dark) / 1)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
