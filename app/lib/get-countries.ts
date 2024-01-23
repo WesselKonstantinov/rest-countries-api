42950e3eabba7f598d03338663018fd1f3bfb738
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-23 23:50:49 +0100

    Create data fetching function

---
+++ app/lib/get-countries.ts
@@ -0,0 +1,9 @@
+export async function getCountries() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
