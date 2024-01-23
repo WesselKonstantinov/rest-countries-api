5e95e43584528348470ceb644e6acde473322a04
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-23 23:54:36 +0100

    Make heading appear first in source order

--- app/ui/country-card.tsx
+++ app/ui/country-card.tsx
@@ -9,21 +9,15 @@ export default function CountryCard({ country }: CountryCardProps) {
   const countryStats = {
     population: country.population,
     region: country.region,
-    capital: country.capital[0],
+    capital: `${country.capital && country.capital.join(", ")}`, // Avoid TypeError
   };
 
   return (
-    <div className="max-w-[264px] bg-light-secondary dark:bg-dark-secondary rounded overflow-hidden shadow-[0_0_0.4375rem_0.125rem_rgba(0,0,0,0.03)]">
-      <div className="relative h-[160px]">
-        <Image
-          src={country.flags.svg}
-          alt={country.flags.alt}
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div className="p-6">
-        <h2 className="text-lg font-extrabold">{country.name.common}</h2>
+    <div className="h-full flex flex-col bg-light-secondary dark:bg-dark-secondary rounded overflow-hidden shadow-[0_0_0.4375rem_0.125rem_rgba(0,0,0,0.03)]">
+      <div className="p-6 order-1">
+        <h2 className="text-lg leading-[1.1] font-extrabold">
+          {country.name.common}
+        </h2>
         <dl className="mt-4 text-sm">
           {Object.entries(countryStats).map(([key, value]) => (
             <div key={key} className="mt-2 flex gap-1">
@@ -38,6 +32,14 @@ export default function CountryCard({ country }: CountryCardProps) {
             </div>
           ))}
         </dl>
+      </div>
+      <div className="relative h-[160px]">
+        <Image
+          src={country.flags.svg}
+          alt={`${country.flags.alt}`}
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
