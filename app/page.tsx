aba5a931b64af21226d2429e37097eb3957f1a34
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-24 19:08:16 +0100

    Enable navigation to country detail pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import { Country } from "./lib/definitions";
-import { getCountries } from "./lib/get-countries";
+import { getCountries } from "./lib/data";
 import CountryCard from "./ui/country-card";
 
 export default async function Home() {
@@ -9,7 +10,9 @@ export default async function Home() {
     <ul className="mx-10 md:mx-0 grid grid-cols-fluid auto-rows-[minmax(21rem,_1fr)] gap-10 lg:gap-18">
       {countries.map((country) => (
         <li key={country.name.common}>
-          <CountryCard country={country} />
+          <Link href={`/country/${country.cca3}`}>
+            <CountryCard country={country} />
+          </Link>
         </li>
       ))}
     </ul>
