698f7bd33b4eeef4da80cae25e061d0b3a646dcd
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-24 14:30:10 +0100

    Filter output using only the specified fields

--- app/lib/get-countries.ts
+++ app/lib/get-countries.ts
@@ -1,5 +1,9 @@
-export async function getCountries() {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+import { Country } from "./definitions";
+
+export async function getCountries(): Promise<Country[]> {
+  const res = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,capital,population,region,flags"
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
