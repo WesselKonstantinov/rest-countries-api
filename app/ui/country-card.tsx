e444526097580948260e86a571bdbe8bed8b68ca
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-25 13:51:08 +0100

    Place keys directly inside HTML instead of dynamically rendering them

--- app/ui/country-card.tsx
+++ app/ui/country-card.tsx
@@ -6,31 +6,27 @@ type CountryCardProps = {
 };
 
 export default function CountryCard({ country }: CountryCardProps) {
-  const countryStats = {
-    population: country.population,
-    region: country.region,
-    capital: country.capital.join(", "),
-  };
-
   return (
     <div className="h-full flex flex-col bg-light-secondary dark:bg-dark-secondary rounded overflow-hidden shadow-[0_0_0.4375rem_0.125rem_rgba(0,0,0,0.03)]">
       <div className="p-6 order-1">
         <h2 className="text-lg leading-[1.1] font-extrabold">
           {country.name.common}
         </h2>
-        <dl className="mt-4 text-sm">
-          {Object.entries(countryStats).map(([key, value]) => (
-            <div key={key} className="mt-2 flex gap-1">
-              <dt className="capitalize font-semibold after:content-[':']">
-                {key}
-              </dt>
-              <dd className="font-light">
-                {typeof value === "number"
-                  ? new Intl.NumberFormat("en-US").format(value)
-                  : value}
-              </dd>
-            </div>
-          ))}
+        <dl className="mt-4 flex flex-col gap-2 text-sm">
+          <div className="flex gap-1">
+            <dt className="font-semibold after:content-[':']">Population</dt>
+            <dd className="font-light">
+              {new Intl.NumberFormat("en-US").format(country.population)}
+            </dd>
+          </div>
+          <div className="flex gap-1">
+            <dt className="font-semibold after:content-[':']">Region</dt>
+            <dd className="font-light">{country.region}</dd>
+          </div>
+          <div className="flex gap-1">
+            <dt className="font-semibold after:content-[':']">Capital</dt>
+            <dd className="font-light">{country.capital.join(", ")}</dd>
+          </div>
         </dl>
       </div>
       <div className="relative h-[160px]">
