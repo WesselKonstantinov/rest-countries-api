a8e6ff6d4eaf87b35a3072e71169f1c08c2b0061
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-26 18:09:21 +0100

    Add search functionality

--- app/ui/search-bar.tsx
+++ app/ui/search-bar.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 type SearchBarProps = {
   placeholder: string;
 };
 
 export default function SearchBar({ placeholder }: SearchBarProps) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative">
       <label htmlFor="search" className="sr-only">
@@ -14,7 +32,9 @@ export default function SearchBar({ placeholder }: SearchBarProps) {
         id="search"
         type="text"
         placeholder={placeholder}
+        defaultValue={searchParams.get("query")?.toString()}
         className="pl-[4.625rem] py-[0.875rem] md:py-[1.125rem] bg-light-secondary dark:bg-dark-secondary rounded-[0.3125rem] shadow-[0_0.125rem_0.5625rem_0_rgba(0,0,0,0.05)] placeholder:text-light-secondary dark:placeholder:text-dark text-xs md:text-sm font-normal"
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="size-4 md:size-[1.125rem] absolute top-1/2 -translate-y-1/2 left-8" />
     </div>
