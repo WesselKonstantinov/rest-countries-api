8db8fad7038a11aad3b2967a8d2e2b5b5e58e1c3
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-31 20:49:22 +0100

    Style region filter

--- app/ui/region-filter.tsx
+++ app/ui/region-filter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function RegionFilter() {
@@ -18,7 +19,7 @@ export default function RegionFilter() {
   };
 
   return (
-    <div>
+    <div className="basis-[12.5rem] relative bg-light-secondary dark:bg-dark-secondary rounded-[0.3125rem] shadow-[0_0.125rem_0.5625rem_0_rgba(0,0,0,0.05)] cursor-pointer">
       <label htmlFor="region-select" className="sr-only">
         Filter by region:
       </label>
@@ -26,6 +27,7 @@ export default function RegionFilter() {
         id="region-select"
         defaultValue={searchParams.get("region")?.toString()}
         onChange={(e) => handleSelect(e.target.value)}
+        className="size-full px-6 py-3.5 md:py-[1.125rem] appearance-none bg-transparent border-none text-xs md:text-sm"
       >
         <option value="">Filter by Region</option>
         <option value="africa">Africa</option>
@@ -34,6 +36,7 @@ export default function RegionFilter() {
         <option value="europe">Europe</option>
         <option value="oceania">Oceania</option>
       </select>
+      <ChevronDownIcon className="size-3 md:size-3.5 absolute top-1/2 -translate-y-1/2 right-[1.125rem]" />
     </div>
   );
 }
